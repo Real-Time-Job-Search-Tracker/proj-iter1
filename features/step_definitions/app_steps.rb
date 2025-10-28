c8346a59e7db9cee28931c2935d911f56cfaaea4
Robert Fornos <rf2830@columbia.edu>
2025-10-28 13:19:58 -0400

    add rough skeleton for step_definitions

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -1,1 +1,27 @@
-# TO-DO
+Given('the parser will return job details for {string}') do |url|
+  # TO-DO
+end
+
+When('I paste {string} into the Add Application form') do |url|
+  # TO-DO
+end
+
+When('I submit the form') do
+  # TO-DO
+end
+
+Then('I should see {string}') do |content|
+  # TO-DO
+end
+
+Then('I should see {string} within the applications list') do |company_name|
+  # TO-DO
+end
+
+Then('I should see the stage {string} for {string}') do |stage, company|
+  # TO-DO
+end
+
+Then('I should not see {string}') do |content|
+  # TO-DO
+end
--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -1,1 +1,19 @@
-# TO-DO
+Given('a user exists with email {string} and password {string}') do |email, password|
+  # TO-DO
+end
+
+When('I visit the sign in page') do
+  # TO-DO
+end
+
+When('I sign in as {string} with password {string}') do |email, password|
+  # TO-DO
+end
+
+Then('I should see {string}') do |content|
+  # TO-DO
+end
+
+Given('I am signed in as {string} with password {string}') do |email, password|
+  # TO-DO
+end
