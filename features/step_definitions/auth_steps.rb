52d3d72e1ae40f37d91b8a3eb08e6c0ca2a2886a
JiawenEvelynLyu <Lyu111@purdue.edu>
2025-10-29 00:32:19 -0400

    Initial Implementation of auth_steps abd sankey_steps

--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -1,19 +1,36 @@
 Given('a user exists with email {string} and password {string}') do |email, password|
   # TO-DO
+  @user = User.find_or_create_by!(email: email) do |u|
+    u.password = password
+    u.password_confirmation = password if u.respond_to?(:password_confirmation)
+  end
 end
 
 When('I visit the sign in page') do
   # TO-DO
+  visit new_user_session_path
 end
 
 When('I sign in as {string} with password {string}') do |email, password|
   # TO-DO
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  click_button 'Log in'
+  @current_user = User.find_by(email: email)
 end
 
 Then('I should see {string}') do |content|
   # TO-DO
+  expect(page).to have_content(content)
 end
 
 Given('I am signed in as {string} with password {string}') do |email, password|
   # TO-DO
+  step %{a user exists with email "#{email}" and password "#{password}"}
+  visit new_user_session_path
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  click_button 'Log in'
+  @current_user = User.find_by(email: email)
+  expect(page).to have_content('Dashboard').or have_content('Signed in successfully')
 end
--- features/step_definitions/sankey_steps.rb
+++ features/step_definitions/sankey_steps.rb
@@ -1,19 +1,34 @@
 Given('an application exists for {string} in stage {string}') do |company, stage_name|
   # TO-DO
+  user = @current_user || User.first || FactoryBot.create(:user)
+  @application = FactoryBot.create(
+    :application,
+    company: company,
+    stage: stage_name,
+    user: user
+  )
 end
 
 When('I request the sankey JSON') do
   # TO-DO
+  visit sankey_path(format: :json)
+  @json_response = JSON.parse(page.body)
 end
 
 Then('the response should be JSON') do
   # TO-DO
+  expect(@json_response).to be_a(Hash)
+  expect(@json_response).to have_key('nodes')
+  expect(@json_response).to have_key('links')
 end
 
 Then('the JSON should include a sankey node for {string}') do |stage_name|
   # TO-DO
+  node_names = @json_response['nodes'].map { |n| n['name'] }
+  expect(node_names).to include(stage_name)
 end
 
 Then('the JSON should include at least 1 link') do
   # TO-DO
+  expect(@json_response['links'].length).to be >= 1
 end
