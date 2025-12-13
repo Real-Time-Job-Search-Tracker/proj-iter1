6533b84cf352231cf13e4797659129c90bd46146
Robert Fornos <rf2830@columbia.edu>
2025-12-12 22:43:28 -0500

    move sign out steps to auth_steps.rb

--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -45,3 +45,22 @@ Given("I am signed in as {string} with password {string}") do |email, password|
   ensure_user(email, password)
   sign_in(email: email, password: password)
 end
+
+When("I sign out") do
+  if page.has_button?("Sign out", wait: 2)
+    click_button("Sign out")
+  else
+    click_link("Sign out")
+  end
+end
+
+Then("I should be signed out") do
+  expect(page).to have_text("Hi, Guest")
+  expect(page).to have_link("Sign in", href: sign_in_path)
+  expect(page).not_to have_button("Sign out")
+end
+
+Then("I should be on the sign in page") do
+  expect([sign_in_path, "/"]).to include(page.current_path)
+  step %(I should be signed out)
+end
