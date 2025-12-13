168f24adf0a481326f30ef90efb77d31530aade6
Robert Fornos <rf2830@columbia.edu>
2025-12-13 00:11:43 -0500

    fix app_steps.rb after refactoring

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -1,125 +1,57 @@
-require 'nokogiri'
-require 'open-uri'
+require "json"
 
-Given("the parser will return job details for {string}") do |url|
-  html = <<~HTML
-    <html>
-      <head>
-        <title>Senior Engineer – ACME</title>
-        <meta property="og:site_name" content="ACME Corp">
-      </head>
-      <body></body>
-    </html>
-  HTML
+When('I paste {string} into the Add Application form') do |url|
+  visit dashboard_path
 
-  stub_request(:get, url).to_return(status: 200, body: html, headers: { "Content-Type" => "text/html" })
-end
+  expect(page).to have_css("form#new_application", wait: 10)
 
-When("I paste {string} into the Add Application form") do |the_url|
-  visit new_application_path
-
-  candidate_fields = [ "application_url", "url", "Final apply URL", "#application_url", "input[name='application[url]']" ]
-
-  field = candidate_fields.find { |name_or_label| page.has_field?(name_or_label, disabled: false, wait: 5) }
-
-  raise 'Could not find the URL field on the Add Application form. Available fields: ' + candidate_fields.join(', ') unless field
-
-  fill_in field, with: the_url
+  find("#application_url", wait: 10).set(url)
 end
 
 When("I submit the form") do
-  # Click the submit button without holding onto a form element
-  if page.has_button?("Add Application", wait: 0.5)
+  if page.has_button?("Add Application", wait: 1)
     click_button "Add Application"
-  elsif page.has_button?("Add", wait: 0.5)
-    click_button "Add"
   else
-    find("input[type=submit], button[type=submit]", match: :first).click
+    find("input[type='submit'][value='Add Application']", wait: 10).click
   end
-
-  page.has_css?(".flash", wait: 5) || page.has_current_path?(/applications|new|jobs/i, wait: 5)
 end
 
-Then('I should see {string}') do |content|
-  # If we just submitted an invalid URL, we expect to be back on the new form.
-  if defined?(new_application_path)
-    begin
-      expect(page).to have_current_path(new_application_path, ignore_query: true, wait: 2)
-    rescue RSpec::Expectations::ExpectationNotMetError
+Then('I should see {string} within the applications list') do |company|
+  if page.has_css?("#applications", visible: :all)
+    within("#applications", visible: :all) do
+      expect(page).to have_css("li", text: company, visible: :all, wait: 10)
     end
+    next
   end
 
-  # Look for flash containers first, with a generous wait
-  if page.has_css?('.flash, .alert, [role="alert"]', text: content, wait: 5, exact_text: false)
-    expect(page).to have_css('.flash, .alert, [role="alert"]', text: content, exact_text: false)
-  else
-    expect(page).to have_text(content, wait: 5)
-  end
-end
-
-Then("I should see {string} within the applications list") do |company|
-  visit applications_path unless page.has_css?("#applications, #applications_list, .applications, .apps", wait: 0.5)
-
-  containers = [ "#applications", "#applications_list", ".applications", ".apps" ]
-  if (container = containers.find { |sel| page.has_css?(sel) })
-    within(container) { expect(page).to have_text(company) }
-  else
-    expect(page).to have_text(company)
-  end
+  expect(page).to have_css("#apps-body", visible: :all, wait: 15)
+  expect(page).to have_css("#apps-body tr", text: company, visible: :all, wait: 15)
 end
 
 Then('I should see the stage {string} for {string}') do |stage, company|
-  app = JobApplication.find_by!(company: company)
-  expect(app.status).to eq(stage)
-end
-
-Then("I should not see {string}") do |content|
-  expect(page).to have_no_text(content, wait: 5)
-end
-
-Given("there is a job application for {string} with title {string} and status {string}") do |company, title, status|
-  JobApplication.create!(
-    company: company,
-    title: title,
-    status: status,
-    url: "https://example.com/#{company.parameterize}"
-  )
-end
-
-When("I update the status of {string} to {string}") do |company, new_status|
-  app = JobApplication.find_by!(company: company)
-  page.driver.put("/applications/#{app.id}", { status: new_status })
-end
-
-Then("the status of {string} should be {string}") do |company, expected_status|
-  app = JobApplication.find_by!(company: company)
-  expect(app.status).to eq(expected_status)
-end
-
-# When("I delete the job application for {string}") do |company|
-# app = JobApplication.find_by!(company: company)
-# page.driver.delete("/applications/#{app.id}")
-# end
-
-# Then("the job application for {string} should not exist") do |company|
-# expect(JobApplication.find_by(company: company)).to be_nil
-# end
-
-When("I delete the job application for {string}") do |company|
-  app = JobApplication.find_by(company: company)
-  raise "No job application found for #{company}" unless app
-
-  if page.current_path.include?("/applications")
-    if page.has_button?("Delete", wait: 0.5)
-      click_button("Delete")
-    else
-      page.driver.delete("/applications/#{app.id}")
+  if page.has_css?("#applications", visible: :all)
+    within("#applications", visible: :all) do
+      li = find("li[data-company='#{company}']", visible: :all, wait: 10)
+      expect(li).to have_css(".stage", text: stage, visible: :all, wait: 10)
     end
-  else
-    page.driver.delete("/applications/#{app.id}")
+    next
   end
+
+  expect(page).to have_css("#apps-body", visible: :all, wait: 15)
+  row = find("#apps-body tr", text: company, visible: :all, wait: 15)
+  expect(row).to have_text(stage)
 end
 
-Then("the job application for {string} should not exist") do |company|
-  expect(JobApplication.find_by(company: company)).to be_nil
+Then("I should not see {string}") do |text|
+  if page.has_css?("#applications", visible: :all)
+    within("#applications", visible: :all) do
+      expect(page).to have_no_css("li", text: text, visible: :all, wait: 10)
+    end
+  end
+
+  if page.has_css?("#apps-body", visible: :all)
+    expect(page).to have_no_css("#apps-body tr", text: text, visible: :all, wait: 10)
+  else
+    expect(page).to have_no_text(text)
+  end
 end
